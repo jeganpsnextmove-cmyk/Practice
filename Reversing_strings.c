1fea8175137f113a034b02a05188160444a876e6
jeganpsnextmove-cmyk <jeganpsnextmove@gmail.com>
2026-02-22 01:46:11 +0530

    Dynamic_memory

--- Reversing_strings.c
+++ Reversing_strings.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<string.h>
+#include <stdlib.h>
 int main()
 {
     int n;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
     getchar();
-    char name[n];
-    char new_name[n];
+    char * name=malloc(n * sizeof(char));
+    char * new_name=malloc(n * sizeof(char));
     printf("Enter the elements of the array: ");
     fgets(name, n, stdin);
     int length=strlen(name);
