0c94d90864566356d4eb1b5d61207c580ce83ef2
jeganpsnextmove-cmyk <jeganpsnextmove@gmail.com>
2026-02-22 20:54:33 +0530

    Word_Reverse_diff_array

--- Reversing_strings.c
+++ Reversing_strings.c
@@ -4,28 +4,40 @@
 int main()
 {
     char name[200];
+    char new_name[200];
+    int n;
+    n=0;
     printf("Enter the elements of the array: ");
     fgets(name, sizeof(name), stdin);
     int length=strlen(name);
-    int end=length-1;
+   
     if(name[length-1]=='\n')
     {
         name[length-1]='\0';
         length--;
     }
+     int end=length-1;
     for(int i=length -1; i>=0; i--)
     {
         if(name[i]==' '|| i==0)
             {
             int start=i==0?0:i+1;
             
-        for(int j=start;j<=end;j++)
-        {
-            printf("%c", name[j]);
-        }
-        printf(" ");
-        end=i-1;
+               for(int j=start;j<=end;j++)
+               {
+               new_name[n++]=name[j];
+               }
+            new_name[n++]=' ';
+            end= i - 1;
+            }
     }
+    if(n>0)
+    {
+        new_name[n-1]='\0';
     }
+
+     // Null-terminate the reversed string
+    printf("The reversed string is: %s\n", new_name);
+    
     return 0;
 }
