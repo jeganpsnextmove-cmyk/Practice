8566b7c073708ce0385b20d4b6877c52099267c5
jeganpsnextmove-cmyk <jeganpsnextmove@gmail.com>
2026-05-02 01:25:12 +0530

    Added Pointer dot product multiplication code

---
+++ dot_product_multiplication.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void multiply(int *a, int *b,int len)
+{
+    int sum=0;
+    for(int i=0;i<len;i++)
+    {
+    sum=sum+*(a+i) * *(b+i);
+    }
+    printf("\n%d",sum);
+}
+
+int main()
+{
+    int a[]={1,2,3};
+    int b[]={4,5,6};
+    int len=sizeof(a)/sizeof(a[0]);
+    printf("the length of a is :%d",len);
+    multiply(a,b,len);
+    return 0;
+}
+
+
+
+
+
+
+
+
